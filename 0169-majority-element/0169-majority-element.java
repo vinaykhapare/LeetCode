30d4ce02d352589e6d7bc82633dc7c44867c74ca
Vinay Khapare <165833551+vinaykhapare@users.noreply.github.com>
2024-08-30 04:39:17 -0700

    Time: 1 ms (99.77%), Space: 52.7 MB (68.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,9 +1,26 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        //Sort the array 
-        Arrays.sort(nums);
-        int n = nums.length;
-        //return the number came n/2 times
-        return nums[n/2];
+        int count = 0;
+        int candidate = 0;
+
+        //Iterate through the nums array
+        for(int num: nums)
+        {
+            //if the array is empty then put the num value in the candidate value
+            if(count == 0)
+            {
+                candidate = num;
+            }
+            //Check the value of the num if it is equal then increament the count not then decreament 
+            if(num == candidate)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return candidate;
     }
 }
