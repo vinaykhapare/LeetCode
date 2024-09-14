1b86dfb9b902065b9d5df9d7c8843002ad3bbac6
Vinay Khapare <165833551+vinaykhapare@users.noreply.github.com>
2024-09-14 21:01:33 +0530

    Time: 5 ms (25.73%), Space: 8.6 MB (29.49%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int ans =0;
+        int n = s.length();
+        for(int i =0; i<n-1; i++)
+        {
+            ans += abs(s[i] - s[i+1]);
+        }
+        return ans;
+    }
+};
