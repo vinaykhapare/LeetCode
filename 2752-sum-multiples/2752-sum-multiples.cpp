af23349fad75aafadee9721f0c9b991c61bf092c
Vinay Khapare <165833551+vinaykhapare@users.noreply.github.com>
2024-09-21 15:24:52 -0700

    Time: 0 ms (100%), Space: 8.3 MB (66.76%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.cpp
+++ 2752-sum-multiples/2752-sum-multiples.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int sumOfMultiples(int n) {
         int ans =0;
-        for(int i=0; i<=n; i++)
+        for(int i=3; i<=n; i++)
         {
             if((i%3 ==0 || i%5 ==0 || i%7 ==0))
             {
