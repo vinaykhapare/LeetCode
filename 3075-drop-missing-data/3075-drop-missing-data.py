cb5b5661e0ab8ade1cd9772c0ecd41f8639887af
Vinay Khapare <165833551+vinaykhapare@users.noreply.github.com>
2024-09-28 15:32:27 +0530

    Time: 357 ms (74.99%), Space: 69.3 MB (91.62%) - LeetHub

---
+++ 3075-drop-missing-data/3075-drop-missing-data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.dropna(subset = ['name'])
+
+    # students = students[students['name'].notna()]
+    # return students
