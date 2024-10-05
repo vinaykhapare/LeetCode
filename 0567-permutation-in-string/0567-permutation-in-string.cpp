2caea3b7669de62081b9a82eb5de6c5d35ddb107
Vinay Khapare <165833551+vinaykhapare@users.noreply.github.com>
2024-10-06 00:24:31 +0530

    Time: 10 ms (41.85%), Space: 8.7 MB (92.55%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    bool solve(string s, string s1)
-    {
-        sort(s.begin(), s.end());
-        sort(s1.begin(), s1.end());
-        return s == s1;
-    }
-    bool checkInclusion(string s1, string s2) 
-    {
-        int n1 = s1.size(), n2 = s2.size();
-        if(n1>n2) return 0;
-        int i =0;
-        while(i <= n2 - n1)
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.length(), m = s2.length();
+        if(n>m) return false;
+
+        vector<int> s1_freq(26,0);
+        vector<int> s2_freq(26,0);
+
+        for(int i =0; i<n; i++)
         {
-            string s = s2.substr(i, s1.size());
-            if(solve(s,s1)) return 1;
-            i++;
+            s1_freq[s1[i] - 'a']++;
         }
-        return 0;
+        int i=0;
+        int j =0;
+        while(j<m)
+        {
+            s2_freq[s2[j] - 'a'] ++;
+
+            if(j-i+1>n)
+            {
+                s2_freq[s2[i] - 'a'] --;
+                i++;
+            }
+            if(s1_freq == s2_freq) return true;
+            j++;
+        }
+        return false;
     }
 };
